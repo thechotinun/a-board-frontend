8375dd627961cb97c3b6bd501c3d0773d4ddae0d
thechotinun <thechotinun@gmail.com>
2024-12-07 01:19:59 +0700

    fix docker-compose, socket

--- src/app/hooks/useSocket.js
+++ src/app/hooks/useSocket.js
@@ -5,7 +5,7 @@ export function useSocket(postId, token) {
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = io(`${process.env.NEXT_PUBLIC_API_URL}/socket-post`, {
+    socket.current = io(`${process.env.NEXT_PUBLIC_SOCKET_URL}/api/v1/socket-post`, {
       extraHeaders: {
         Authorization: `Bearer ${token}`,
       },
